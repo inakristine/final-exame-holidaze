de54f0a75e02878b62bc5541f15d708849d72685
Ina Kristine <inakristine@gmail.com>
2020-06-11 12:15:09 +0200

    Build

---
+++ holidaze-client/build/precache-manifest.45a920b8df4bfcbe08576fbe163235d2.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e130bbd411ae8e927f1939e9d509049",
+    "url": "/holidaze/index.html"
+  },
+  {
+    "revision": "af6541593b15d49a9d4c",
+    "url": "/holidaze/static/css/main.0a86c3f9.chunk.css"
+  },
+  {
+    "revision": "a8d5be7c577c318aee6a",
+    "url": "/holidaze/static/js/2.8bda51c3.chunk.js"
+  },
+  {
+    "revision": "d2cac4d69c637e42f47b21733a6c2f53",
+    "url": "/holidaze/static/js/2.8bda51c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af6541593b15d49a9d4c",
+    "url": "/holidaze/static/js/main.9922f3ae.chunk.js"
+  },
+  {
+    "revision": "b55bb97ebeaf1775e363",
+    "url": "/holidaze/static/js/runtime-main.7b4a6013.js"
+  },
+  {
+    "revision": "25400c0d9660ae180fa707fe382c7b33",
+    "url": "/holidaze/static/media/ad-holidaze.25400c0d.jpg"
+  },
+  {
+    "revision": "19e4b017506ebccf8556f26ffe527253",
+    "url": "/holidaze/static/media/bkgr-holidaze-mod.19e4b017.jpg"
+  },
+  {
+    "revision": "f21f7e012969ddbd6b7333623ea94cca",
+    "url": "/holidaze/static/media/harbour.f21f7e01.jpg"
+  },
+  {
+    "revision": "52beb089fd914896e32d5c017ed8e496",
+    "url": "/holidaze/static/media/streets.52beb089.jpg"
+  }
+]);
